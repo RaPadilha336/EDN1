dd4cf80b8c001ead6f015007da3213ce24148d7b
Rafaela Pereira Padilha <141189007+RaPadilha336@users.noreply.github.com>
2025-10-29 21:42:16 -0300

    Update calculadoraDeNumeroInteiro.py

--- calculadoraDeNumeroInteiro.py
+++ calculadoraDeNumeroInteiro.py
@@ -1,9 +1,10 @@
-A = int(input())
-B = int(input())
-C = int(input())
-D = int(input())
+A = int(input("Digite o valor inteiro de A: "))
+B = int(input("Digite o valor inteiro de B: "))
+C = int(input("Digite o valor inteiro de C: "))
+D = int(input("Digite o valor inteiro de D: "))
 
 DIFERENCA = (A * B - C * D)
 print("DIFERENCA =", DIFERENCA)
+
 
 
